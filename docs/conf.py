bb773e9b542abbf5a56972f78abbaca3588aa5cc
Boohja <matthias@comasu.de>
2021-06-10 14:24:28 +0200

    missing conf

--- docs/conf.py
+++ docs/conf.py
@@ -43,6 +43,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    'sphinxcontrib.mermaid',
+    'sphinxcontrib.httpdomain'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
